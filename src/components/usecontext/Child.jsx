09aaea6690d8eabaeb9212aebe3e706b55016ebb
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-03-01 10:39:10 +0530

    first commit

---
+++ src/components/usecontext/Child.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useGlobalContext } from "./userContext";
+
+const Child = () => {
+  // const { name, age } = user;
+  const userData = useGlobalContext();
+  // console.log("🚀 ~ file: Child.jsx ~ line 8 ~ Child ~ userData", userData);
+
+  return (
+    <div>
+      Child = My name is {userData.name} and my age is {userData.age}.{" "}
+    </div>
+  );
+};
+
+export default Child;
